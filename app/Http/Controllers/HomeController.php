978207f12863b22eafb1cf0bd9acdc09cb4fd84c
IZANA <fenomenal11>
2024-05-18 23:48:30 -0500

    mi primer proyecto

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\DocenteController;
+use App\Http\Controllers\EstudianteController;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $users = User::count();
+
+        $widget = [
+            'users' => $users,
+            //...
+        ];
+
+        return view('home', compact('widget'));
+    }
+}
